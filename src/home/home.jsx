465880281a54e188aaa6a92fff4fd95beb05ee50
savannah bjorkman <savbjork@byu.edu>
2025-03-16 20:37:26 -0600

    add third party

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './home.css';
 
+const api_key = import.meta.env.VITE_REACT_APP_CALENDARIFIC_AUTH;
+
 export function Home({ userName, token }) {
   const [availFriend, setAvailFriend] = useState([]);
   const [busyFriend, setBusyFriend] = useState([]);
+  const [holiday, setHoliday] = useState('');
 
   useEffect(() => {
     updateFriends(userName); // Fetch friends initially
@@ -11,6 +14,47 @@ export function Home({ userName, token }) {
     return () => clearInterval(interval);
     
   }, [userName, token]);
+
+  useEffect(() => {
+    fetchHoliday();
+    const interval = setInterval(() => fetchHoliday(), 86400000); // every day
+    return () => clearInterval(interval);
+  }, []);
+
+  async function fetchHoliday() {
+    console.log({api_key});
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const day = today.getDate(); //TODO: change to today
+    const year = today.getFullYear();
+    const url = `https://calendarific.com/api/v2/holidays?&api_key=${api_key}&country=US&year=${year
+    }&day=${day}&month=${month}`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      console.log("Response status: " + response.status);
+      const data = await response.json();
+
+      console.log(url);
+      console.log("Response status: " + data.status);
+      console.log(data);
+
+      if (data.response && data.response.holidays && data.response.holidays.length > 0) {
+        setHoliday(data.response.holidays[0].name);
+      } else {
+        setHoliday('');
+      }
+
+    } catch (error) {
+      console.error("Error updating friend statuses:", error);
+    }
+  }
 
   async function updateFriends(user) {
     try {
@@ -37,7 +81,7 @@ export function Home({ userName, token }) {
         <li className="list-group-item" key={friend}>{friend}</li>
       ));
 
-      setAvailFriend(avail.length > 0 ? avail : [<li className="list-group-item" key="none">No friends available</li>]);
+      setAvailFriend(avail);
       setBusyFriend(busy);
     } catch (error) {
       console.error("Error updating friend statuses:", error);
@@ -68,6 +112,20 @@ export function Home({ userName, token }) {
             </ul>
           </div>
         </div>
+        <br />
+        {holiday.length > 0 && (
+          <>
+            <h4 className="text-center">Need a reason to call?</h4>
+            <h4 className="text-center">It is {holiday}!</h4>
+            <br />
+          </>
+        )}
+        {holiday.length === 0 && (
+          <>
+            <h4 className="text-center">No holiday today, just call to say hi!</h4>
+            <br />
+          </>
+        )}  
       </div>
     </main>
   );
