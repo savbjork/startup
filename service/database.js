bbfae7ac6f4510d2229b098d681062d7e5034109
savannah bjorkman <savbjork@byu.edu>
2025-04-01 22:22:57 -0600

    fixed status prob

--- service/database.js
+++ service/database.js
@@ -72,27 +72,29 @@ async function deleteAvailabilityWeekly(token, availability) {
 
 async function getStatus(name) {
   // const availNow = await userCollection.findOne({ name: name }, { projection: { availNow: 1 } });
-  const now = new Date(new Date().toLocaleString("en-US", { timeZone: "UTC" }));
+  //const now = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Denver" }));
+  const now = new Date();
   // if (availNow && availNow > now) {
   //   return "AVAILABLE";
   // }
   const availWeekly = await userCollection.findOne({ email: name }, { projection: { availWeekly: 1 } });
+  const today = now.toISOString().split('T')[0];
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const todayDayOfWeek = daysOfWeek[now.getDay()]; // Get the current day of the week as a name
   if (availWeekly) {
-    console.log(availWeekly);
-    console.log(availWeekly.availWeekly.length);
     for (const availability of availWeekly.availWeekly) {
-      const today = now.toISOString().split('T')[0];
-      const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      const todayDayOfWeek = daysOfWeek[now.getDay()]; // Get the current day of the week as a name
       if (availability.day === todayDayOfWeek) {
-      const start = new Date(`${today}T${availability.start}:00Z`);
-      const end = new Date(`${today}T${availability.end}:00Z`);
-      console.log(start);
-      console.log(end);
-      console.log(now);
-      if (start <= now && now <= end) {
-        return {status: "AVAILABLE"};
-      }
+        const start = new Date(now);
+        start.setHours(availability.start.split(':')[0], availability.start.split(':')[1], 0, 0);
+        const end = new Date(now);
+        end.setHours(availability.end.split(':')[0], availability.end.split(':')[1], 0, 0);
+        //const end = new Date(`${today}T${availability.end}:00`);
+        console.log(start);
+        console.log(end);
+        console.log(now);
+        if (start <= now.getTime() && now.getTime() <= end) {
+          return {status: "AVAILABLE"};
+        }
       }
     }
   }
